2bc3ea58aef2f8f2ea99cdb92ba560c5796e101d
Gerardo Aldana <legitgeo@gmail.com>
2021-04-15 21:19:33 -0700

    Initial Commit

---
+++ Team 2 To Do List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Team 2 To Do List
+//
+//  Created by Geo Aldana on 4/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
