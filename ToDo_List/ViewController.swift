b4268d2bb82d38cc217292047df1def441f148b9
Gerardo Aldana <legitgeo@gmail.com>
2021-04-16 21:09:08 -0700

    started project and planned some things out

--- ToDo_List/ViewController.swift
+++ ToDo_List/ViewController.swift
@@ -5,13 +5,47 @@
 //  Created by Geo Aldana on 4/16/21.
 //
 
+import RealmSwift
 import UIKit
 
-class ViewController: UIViewController {
+class ToDoListItem: Object {
+    @objc dynamic var item: String = ""
+    @objc dynamic var date: Date = Date()
+}
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet var table: UITableView!
+    
+    private var data = [ToDoListItem]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        table.delegate = self
+        table.dataSource = self
+    }
+    
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return data.count
+        }
+
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+            cell.textLabel?.text = data[indexPath.row].item
+            return cell
+        }
+
+        func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            tableView.deselectRow(at: indexPath, animated: true)
+
+            // Open the screen where we can see item info and dleete
+            
+            }
+
+    @IBAction func didTapADDButton() {
+        
     }
 
 
